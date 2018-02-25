31d0483e205ea322f29df1eae2f9fb70583fa9cf
Muath Jafari <muath.jafari@gmail.com>
2018-02-25 17:03:38 +0200

    add missing config

--- src/main/java/com/aliens/ClientController.java
+++ src/main/java/com/aliens/ClientController.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,15 +14,9 @@ import java.util.stream.Collectors;
 @Controller
 public class ClientController {
 
-    @Autowired
-    DiscoveryClient discoveryClient;
-
-    @GetMapping("hi")
-    public String sayHi(){
-
-        List<ServiceInstance> instances = discoveryClient.getInstances("eureka-client-two");
-        String collect = instances.stream().map(i -> i.getUri().toString()).collect(Collectors.joining());
-        return collect;
+    @GetMapping("/hello")
+    public @ResponseBody String sayHello(){
+        return "hello world";
     }
 
 }
